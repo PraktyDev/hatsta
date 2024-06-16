045536261944405cbc5934414558b94ca8ab8f77
Praise Akintayo <prakty1205@gmail.com>
2024-06-16 03:14:42 -0700

    updated form component to post data to database

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,19 +1,3 @@
-// import { ConnectToDB } from "../../../../utils/database";
-// import { Event } from "../../../../models/Event";
-
-// export async function POST (req){
-//     const body = await req.json()
-//     try {
-//         await ConnectToDB();
-//         const event = await Event.create(body)
-//         return Response.json(event)
-//     } catch (error) {
-//         return Response.json('Error Occured:', error.message)
-//     }
-// }
-
-
-
 import { NextResponse } from "next/server";
 import { Product } from "../../../../models/Product";
 import { ConnectToDB } from "../../../../utils/database";
@@ -22,7 +6,7 @@ export async function POST (request){
     const body = await request.json()
     const { title, description, amount, image, publicId } = body
     if(!image || !publicId){
-        return new NextResponse({ message: 'Error Occured: image url and publicId required' }, { status: 500 })
+        return new NextResponse({ message: 'Error Occured: image url and publicId required' }, { status: 400 })
     }
     try {
         await ConnectToDB()
@@ -34,44 +18,11 @@ export async function POST (request){
             publicId,
         })
         const product = await products.save()
-        console.log(product)
         return new NextResponse(JSON.stringify(product), { message: "Post Successfully"}, { status: 200 })
     } catch (error) {
         return new NextResponse('Error Occured:' + error.message, { status: 500 })
     }
 }
-
-// // export async function POST (req, res){
-// //     const body = await req.json()
-// //     try {
-// //         await connectToDB();
-// //         const product = await Product.create(body)
-// //         return new Response(JSON.stringify(product))
-// //     } catch (error) {
-// //         return new Response.json('Error Occured:', error.message)
-// //     }
-// // }
-
-// export async function POST(request) {
-//     try {
-//         const body = await request.formData();
-//         const { title, description, amount } = body
-//         console.log('Received payload:', body);
-//         // Process the body here
-//         await connectToDB();
-//         const products = new Product({
-//         title,
-//         description,
-//         amount,
-//         })
-//         const product = await products.save()
-//         return new Response(JSON.stringify((product), { message: 'Post Successfully' }, { status: 200 }));
-//     } catch (error) {
-//         console.error('Error parsing JSON:', error);
-//         return new Response(JSON.stringify({ error: 'Invalid JSON' }, { status: 500 }));
-//     }
-// }
-
 
 
 export async function GET (request){
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,22 +6,12 @@ import { CldUploadButton } from 'next-cloudinary';
 import Image from "next/image"
  
 const Form = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    amount: '',
-  })
-
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState('')
   const [image, setImage] = useState('')
   const [publicId, setPublicId] = useState('')
 
-  const handleChange = (e) =>{
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-  }
 
   const handleImageUpload = (result) =>{
     const info = result.info;
@@ -52,16 +42,11 @@ const Form = () => {
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
-    const formDataToSend = new FormData();
-    formDataToSend.append("title", formData.title)
-    formDataToSend.append("description", formData.description)
-    formDataToSend.append("amount", formData.amount)
-    formDataToSend.append("image", image)
-    formDataToSend.append("publicId", publicId)
+    const formData = { title, description, amount, image, publicId }
     try {
       const response = await fetch("/api/products",{
         method: 'POST',
-        body: JSON.stringify(formDataToSend)
+        body: JSON.stringify(formData)
       })
       if(!response.ok){
         throw new Error("Failed to Post")
@@ -77,22 +62,22 @@ const Form = () => {
       <form onSubmit={handleSubmit} className="flex flex-col gap-12">
         <div className="flex flex-col gap-4 items-center ">
           <div className="relative w-full flex items-center">
-            <input name="title" value={formData.title} onChange={handleChange} className="border border-black pl-10  w-full py-2 px-4 rounded-md" type="text" placeholder="Product Title"/>            
+            <input name="title" value={title} onChange={(e)=>setTitle(e.target.value)} className="border border-black pl-10  w-full py-2 px-4 rounded-md" type="text" placeholder="Product Title"/>            
             <div className="absolute left-4 text-blue-400"><FaRegUser /></div>
           </div>        
           <div className="relative w-full flex items-center">
-            <input name="description" value={formData.description} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="Product Description" />            
+            <input name="description" value={description} onChange={(e)=>setDescription(e.target.value)} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="Product Description" />            
             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
           </div>  
           <div className="relative w-full flex items-center">
-            <input name="amount" value={formData.amount} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="Product Amount" />            
+            <input name="amount" value={amount} onChange={(e)=>setAmount(e.target.value)} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="Product Amount" />            
             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
           </div>
           <CldUploadButton uploadPreset= {process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET} onUpload={handleImageUpload} className={`h-48 w-full border-2 mt-4 border-dotted grid place-items-center bg-slate-100 rounded-md relative ${image && 'pointer-events-none'}`}>
             Add Image
             {image && (<Image src={image} fill className="absolute object-cover inset-0" alt="product" /> )}
           </CldUploadButton>
-          {publicId && (<button onClick={removeImage}>Remove Image</button>)}                         
+          {publicId && (<button onClick={removeImage} className="rounded-lg py-1 px-3 bg-red-500 hover:bg-red-400 text-white">Remove Image</button>)}                         
         </div>
 
         <div className="flex flex-col gap-6 items-center">
@@ -104,99 +89,3 @@ const Form = () => {
 }
 
 export default Form
-
-
-// 'use client'
-// import { useState } from "react";
-// import { FaRegUser } from "react-icons/fa";
-// import { FiMail } from "react-icons/fi";
-// import { MdOutlineLock } from "react-icons/md";
-
-// const Form = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     location: '',
-//     amount: '',
-//     date: '',
-//     time: '',
-//     description1: '',
-//     description2: '',
-//     description3: '',
-//     image: '',
-//   })
-
-//   const handleChange = (e) =>{
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     })
-//   }
-
-//   const handleSubmit = async (e) =>{
-//     e.preventDefault();
-//     try {
-//       const response = await fetch("/api/products",{
-//         method: 'POST',
-//         body: JSON.stringify(formData)
-//       })
-//       if(!response.ok){
-//         throw new Error("Failed to Post")
-//       }
-//       return response.json({ message: "Posted Successfully" })
-//     } catch (error) {
-//       console.error('Error:', error.message)
-//     }
-//   }
-
-//   return (
-//     <section className="p-4 mt-20 h-full max-w-md mx-auto flex flex-col border border-blue-200 justify-between">
-//       <form onSubmit={handleSubmit} className="flex flex-col gap-12">
-//         <div className="flex flex-col gap-4 items-center ">
-//           <div className="relative w-full flex items-center">
-//             <input name="name" value={formData.name} onChange={handleChange} className="border border-black pl-10  w-full py-2 px-4 rounded-md" type="text" placeholder="Event name"/>            
-//             <div className="absolute left-4 text-blue-400"><FaRegUser /></div>
-//           </div>
-//           <div className="relative w-full flex items-center">
-//             <input name="location" value={formData.location} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="Location"/>            
-//             <div className="absolute left-4 text-blue-400"><FiMail /></div>
-//           </div>
-//           <div className="relative w-full flex items-center">
-//             <input name="amount" value={formData.amount} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="Entry amount" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//           <div className="relative w-full flex items-center">
-//             <input name="date" value={formData.date} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="date" placeholder="date" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//           <div className="relative w-full flex items-center">
-//             <input name="time" value={formData.time} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="time" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//           <div className="relative w-full flex items-center">
-//             <input name="description1" value={formData.description1} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="description for regular" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//           <div className="relative w-full flex items-center">
-//             <input name="description2" value={formData.description2} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="description for VIP" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//           <div className="relative w-full flex items-center">
-//             <input name="description3" value={formData.description3} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="description for table for 5" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//           <div className="relative w-full flex items-center">
-//             <input name="image" value={formData.image} onChange={handleChange} className="border border-black pl-10 w-full py-2 px-4 rounded-md" type="text" placeholder="image url" />            
-//             <div className="absolute left-4 text-blue-400"><MdOutlineLock /></div>
-//           </div>          
-//         </div>
-
-//         <div className="flex flex-col gap-6 items-center">
-//           <button className="bg-blue-800 hover:bg-blue-700 text-white w-full rounded-md py-2 ">Add Event</button>
-//         </div>
-//       </form>
-//     </section>
-//   )
-// }
-
-// export default Form
