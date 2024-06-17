b1202b755c28c75fd37adbf7ca12decbf5541cda
Praise Akintayo <prakty1205@gmail.com>
2024-06-17 10:03:39 -0700

    added some lines of code for deployment

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import Hero2 from "@/components/Hero2";
 import Hero3 from "@/components/Hero3";
 
 export default function Home() {
+  if(!process.env.NEXT_PUBLIC_API_URL){
+    return null;
+  }
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 p-9 laptop:p-10">
         <Hero1 />
